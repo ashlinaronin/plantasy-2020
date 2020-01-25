27feabcd35c124c6d2f60b68788912442cd3d25c
ashlinaronin <ashlinaronin@gmail.com>
2020-01-24 22:50:12 -0800

    adapt teensy sketch based on specs from rotary angle sensor-- much more accurate

--- teensy_knob/teensy_knob.ino
+++ teensy_knob/teensy_knob.ino
@@ -1,21 +1,51 @@
-int knobValue; 
-int MOTOR_WRITE_PIN = 16;
-int KNOB_READ_PIN = 14;
+/*macro definitions of Rotary angle sensor and LED pin*/
+#define ROTARY_ANGLE_SENSOR A0
+#define LED 3 //the Grove - LED is connected to D3 of Arduino
+#define ADC_REF 5 //reference voltage of ADC is 5v.If the Vcc switch on the seeeduino
+//board switches to 3V3, the ADC_REF should be 3.3
+#define GROVE_VCC 5 //VCC of the grove interface is normally 5v
+#define FULL_ANGLE 300 //full value of the rotary angle is 300 degrees
+
+#define MOTOR_WRITE_PIN 16
+#define KNOB_READ_PIN 14
+
+int knobValue;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(KNOB_READ_PIN, INPUT);
+  pinMode(MOTOR_WRITE_PIN, OUTPUT);
 }
 
 void loop() {
-  int knobValue = analogRead(KNOB_READ_PIN);
+  int degrees = getDegrees();
+  Serial.print("The angle between the mark and the starting position:");
+  Serial.println(degrees);
+
+  // map to output range
+  int speed = map(degrees, 0, FULL_ANGLE, 0, 255);
+  Serial.print("The fan speed:");
+  Serial.println(speed);
+  controlFan(speed);
 
   // send value from knob over serial to processing
   Serial.println(knobValue);
 
-  // write value from knob to motor control
-  // analog writes are in range 0-255,
-  // analog ins are in range 0-1023
-  analogWrite(MOTOR_WRITE_PIN, knobValue /4);
-
   delay(100);
+}
+
+void controlFan(int speed) {
+  analogWrite(MOTOR_WRITE_PIN, speed);  
+}
+
+/************************************************************************/
+/*Function: Get the angle between the mark and the starting position  */
+/*Parameter:-void                           */
+/*Return: -int,the range of degrees is 0~300              */
+int getDegrees()
+{
+  int sensor_value = analogRead(ROTARY_ANGLE_SENSOR);
+  float voltage = (float)sensor_value * ADC_REF / 1023;
+  float degrees = (voltage * FULL_ANGLE) / GROVE_VCC;
+  return degrees;
 }
