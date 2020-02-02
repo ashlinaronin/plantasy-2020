aafd0d2c48d3af19830df014bc5bdbc63b017a3d
ashlinaronin <ashlinaronin@gmail.com>
2020-02-01 20:25:38 -0800

    Update motor driver script now that I actually understand how it works

--- velleman_vma409/velleman_vma409.ino
+++ velleman_vma409/velleman_vma409.ino
@@ -1,37 +1,34 @@
 // trying to use the velleman vma409 motor driver to drive the dc brushless fan
 // fan - DC 12V 1.5A, DFS923212MD0T
 
+#define ROTARY_ANGLE_SENSOR 14
+#define FULL_ANGLE 300 //full value of the rotary angle is 300 degrees
+#define ADC_REF 5 //reference voltage of ADC is 5v.If the Vcc switch on the seeeduino
+//board switches to 3V3, the ADC_REF should be 3.3
+#define GROVE_VCC 5 //VCC of the grove interface is normally 5v
 
+// todo use #define?
 int in1 = 11;
 int in2 = 12;
-//int in3 = 22;
-//int in4 = 23;
-
+int knobReadPin = 14;
 int speedPinA = 6;
-int speedPinB = 5;
 
 void setup() {
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-  //pinMode(in3, OUTPUT);
-  //pinMode(in4, OUTPUT);
+  pinMode(speedPinA, OUTPUT);
 
-  digitalWrite(in1, HIGH);
+  digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
-  //digitalWrite(in3, HIGH);
-  //digitalWrite(in4, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  _mRight(in1, in2);
-  //_mRight(in3, in4);
-
-  _mSetSpeed(
-
-  //int n = analogRead(14) / 4;
-  //_mSetSpeed(speedPinA, n);
-  //_mSetSpeed(speedPinB, n);
+  //_mRight(in1, in2);
+  int deg = getMappedDegrees();
+  Serial.println(deg);
+  
+  _mSetSpeed(speedPinA, deg);
+  delay(20);
 }
 
 void _mRight(int pin1, int pin2) {
@@ -51,4 +48,20 @@ void _mStop(int pin1, int pin2) {
 
 void _mSetSpeed(int pinPWM, int speedValue) {
   analogWrite(pinPWM, speedValue);  
+}
+
+int getMappedDegrees() {
+  return map(getDegrees(), 0, FULL_ANGLE, 0, 255);
+}
+
+
+/************************************************************************/
+/* Function: Get the angle between the mark and the starting position   */
+/* Parameter: void                                                      */
+/* Return: int, the range of degrees is 0~300                           */
+int getDegrees()
+{
+  int sensor_value = analogRead(knobReadPin);
+  float voltage = (float)sensor_value * ADC_REF / 1023;
+  return (voltage * FULL_ANGLE) / GROVE_VCC;
 }
